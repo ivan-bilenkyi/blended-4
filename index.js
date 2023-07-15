d140c6f4ec13563b314147d1ba791d7a03899fc7
Dmytro Shkatula <idnol84@gmail.com>
2023-07-15 15:35:20 +0300

    task-2

--- index.js
+++ index.js
@@ -7,11 +7,25 @@
  *?     в противном случае отобразить:"Не знаете? ECMAScript!"
  */
 
- let message = prompt("Какое официальное название JavaScript?");
-let answer = "ECMAScript".toLocaleLowerCase();
-    message = message.toLocaleLowerCase().trim();
- if(message === answer){
-    alert("Верно!")
- } else {
-    alert("Не знаете? ECMAScript!");
- }
+// let message = prompt("Какое официальное название JavaScript?");
+// let answer = "ECMAScript".toLocaleLowerCase();
+// message = message.toLocaleLowerCase().trim();
+//  if(message === answer){
+//     alert("Верно!")
+//  } else {
+//     alert("Не знаете? ECMAScript!");
+//  }
+
+// Напишіть програму, яка отримає від користувача
+// число (кількість хвилин) та виведе в консоль
+// рядок у форматі годин та хвилин
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+//     70 === 01:10
+
+// let userAnswer = prompt('Введіть число');
+//
+// let hours = String(Number.parseInt(userAnswer/60));
+// hours = hours.padStart(2 , 0);
+// let min = String(userAnswer % 60);
+// min = min.padStart(2 , 0);
+// console.log(`${hours}:${min}`);
